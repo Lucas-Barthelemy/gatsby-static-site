81cbbec7852eeca761275cf5eb90304f2a53b601
LucasBart <lucasbart@hotmail.fr>
2023-07-04 10:11:49 +0200

    configs

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,14 +9,20 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins:
+  [
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options:
+      {
+        name: "blogs",
+        path: `${__dirname}/content/blogs`
+      }
     },
-    __key: "pages"
-  }]
+    "gatsby-transformer-remark"
+  ]
 };
 
 export default config;
